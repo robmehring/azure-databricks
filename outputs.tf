687b93adefd3cdb7f16db781a5ff8ae1caf49210
robmehring <robmehring@gmail.com>
2025-03-19 14:41:21 -0400

    added outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "azure_databricks_workspace_id" {
-  value = azurerm_databricks_workspace.databricks_workspace.id
+  description = "The ID of the Databricks Workspace in the Azure management plane."
+  value       = azurerm_databricks_workspace.databricks_workspace.id
 }
 
 output "databricks_workspace_id" {
-  value = azurerm_databricks_workspace.databricks_workspace.workspace_id
+  description = "The unique identifier of the databricks workspace in Databricks control plane."
+  value       = azurerm_databricks_workspace.databricks_workspace.workspace_id
 }
