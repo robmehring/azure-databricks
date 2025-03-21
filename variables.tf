3b12c12f4e0f8ce2ba742758e223f0c0b01d48bc
robmehring <robmehring@gmail.com>
2025-03-20 20:03:25 -0400

    added default value to managed resource group name variable

--- variables.tf
+++ variables.tf
@@ -10,8 +10,8 @@ variable "location" {
 
 variable "managed_resource_group_name" {
   description = "(Optional) The name of the resource group where Azure should place the managed Databricks resources. Defaults to databricks-rg-<resource group name>. Changing this forces a new resource to be created."
-  default = null
-  type = string
+  default     = null
+  type        = string
 }
 
 variable "public_network_access_enabled" {
